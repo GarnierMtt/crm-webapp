f5481882f86c696837b4818ccee3cb073748677a
Garnier.mtt <garnier.mtt@gmail.com>
2026-05-28 17:00:22 +0200

    guive current user

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,9 +2,13 @@
 
 namespace App\Controller;
 
+use App\Entity\Utilisateurs;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\CurrentUser;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
@@ -46,5 +50,22 @@ class SecurityController extends AbstractController
     public function logout(): void
     {
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    }
+
+
+
+
+    
+    #[Route(path: '/userInfo', name: 'api_userInfo')]
+    public function userInfo(#[CurrentUser] Utilisateurs $user,NormalizerInterface $normalizer): Response
+    {
+        return new JsonResponse(
+            $normalizer->normalize($user, 'object', array(AbstractNormalizer::IGNORED_ATTRIBUTES => ['password'])), 
+            Response::HTTP_OK, 
+            [], 
+            false)
+        ->setEncodingOptions(
+            JSON_UNESCAPED_UNICODE | 
+            JSON_UNESCAPED_SLASHES);
     }
 }
